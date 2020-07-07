855833b0bd428b1604e108cc9884ebfc7af2d507
Carles Capellas <capellas.carles@gmail.com>
2020-07-07 19:37:17 +0200

    feat: add responsive max-width

--- src-client/components/section-container.tsx
+++ src-client/components/section-container.tsx
@@ -7,7 +7,9 @@ export interface SectionContainerProps {
 
 export const SectionContainer: React.FC<SectionContainerProps> = (props) => (
     <div className={props.sectionName}>
-        <div className="section-content">{props.children ? props.children : null}</div>
+        <div className="section-viewport">
+            <div className="section-content">{props.children ? props.children : null}</div>
+        </div>
         <div className="section-links">{props.links}</div>
     </div>
 );
