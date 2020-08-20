1391509e7256073fba3ea408fddbfbd2dfc52003
Carles Capellas <capellas.carles@gmail.com>
2020-08-20 15:11:57 +0200

    feat: add service worker to serve page offline

---
+++ assets/sw.js
@@ -0,0 +1,47 @@
+const myCache = 'CARLES_CAPELLAS_REACT_CACHE';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        (async function () {
+            const cache = await caches.open(myCache);
+            await cache.addAll([
+                '/',
+                '/favicon.png?$modena=react-personal-page',
+                '/main.css?$modena=react-personal-page',
+                '/client-bundle.js?$modena=react-personal-page'
+            ]);
+            // TODO Add rest of resources (eg. blog images, portfolio, etc.) with out await
+        })()
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        (async function () {
+            const cache = await caches.open(myCache);
+            const cacheNames = await cache.keys();
+            await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+        })()
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        (async function () {
+            const networkPromise = fetch(event.request);
+            const cachePromise = caches.open(myCache);
+
+            try {
+                const networkResponse = await networkPromise;
+                cachePromise.then((cache) => {
+                    cache.put(event.request, networkResponse);
+                });
+                return networkResponse.clone();
+            } catch (error) {
+                const cache = await cachePromise;
+                const cacheResponse = await cache.match(event.request);
+                return cacheResponse;
+            }
+        })()
+    );
+});
