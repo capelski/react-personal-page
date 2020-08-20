e24f2f9eb6410d442ffbb74a50a3832348515aa2
Carles Capellas <capellas.carles@gmail.com>
2020-08-20 15:25:48 +0200

    fix: add missing alt attributes for project image

--- src-client/components/project.tsx
+++ src-client/components/project.tsx
@@ -27,7 +27,10 @@ export const Project: React.FC<ProjectProps> = (props) => (
         </div>
         {props.children ? props.children : null}
         <div className="project-image-wrapper">
-            <img src={`/images/portfolio/${props.image}?$modena=react-personal-page`} />
+            <img
+                src={`/images/portfolio/${props.image}?$modena=react-personal-page`}
+                alt={`${props.title} project`}
+            />
             {props.url ? (
                 <a target="_blank" href={props.url} className="project-demo">
                     ▶️
